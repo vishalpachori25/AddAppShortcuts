deb60e6be0a78bcf8c9757961913a3b1a1c08c0a
Vishal Pachori <vishalpachori25@gmail.com>
2020-02-04 21:04:43 +0530

    Add project files.

---
+++ AddShortcuts/AddShortcuts.Android/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Prism;
+using Prism.Ioc;
+using Android.Content;
+using Plugin.AppShortcuts;
+
+namespace AddShortcuts.Droid
+{
+    [Activity(Label = "AddShortcuts", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [IntentFilter(new[] { Intent.ActionView },
+                  Categories = new[] { Intent.CategoryDefault },
+                  DataScheme = "myscheme",
+                  DataHost = "xamarinappshortcuts",
+                  AutoVerify = true)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            //call this before LoadApplication
+            CrossAppShortcuts.Current.Init();
+
+            LoadApplication(new App(new AndroidInitializer()));
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any platform specific implementations
+        }
+    }
+}
+
---
+++ AddShortcuts/AddShortcuts/App.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Prism;
+using Prism.Ioc;
+using AddShortcuts.ViewModels;
+using AddShortcuts.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.AppShortcuts;
+using System.Linq;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AddShortcuts
+{
+    public partial class App
+    {
+        public const string AppShortcutUriBase = "myscheme://xamarinappshortcuts/";
+        public const string ShortcutOption1 = "PAGE1";
+        public const string ShortcutOption2 = "PAGE2";
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            AddShortcuts();
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+        async void AddShortcuts()
+        {
+            if (CrossAppShortcuts.IsSupported)
+            {
+                var shortCurts = await CrossAppShortcuts.Current.GetShortcuts();
+                if (shortCurts.FirstOrDefault(prop => prop.Label == "Detail") == null)
+                {
+                    var shortcut = new Shortcut()
+                    {
+                        Label = "Detail",
+                        Description = "Go to Detail",
+                        Icon = new ContactIcon(),
+                        Uri = $"{AppShortcutUriBase}{ShortcutOption1}"
+                    };
+                    await CrossAppShortcuts.Current.AddShortcut(shortcut);
+                }
+
+                if (shortCurts.FirstOrDefault(prop => prop.Label == "Detail 2") == null)
+                {
+                    var shortcut = new Shortcut()
+                    {
+                        Label = "Detail 2",
+                        Description = "Go to Detail 2",
+                        Icon = new UpdateIcon(),
+                        Uri = $"{AppShortcutUriBase}{ShortcutOption2}"
+                    };
+                    await CrossAppShortcuts.Current.AddShortcut(shortcut);
+                }
+            }
+        }
+
+        protected override void OnAppLinkRequestReceived(Uri uri)
+        {
+            var option = uri.ToString().Replace(AppShortcutUriBase, "");
+            if (!string.IsNullOrEmpty(option))
+            {
+                MainPage = new NavigationPage(new MainPage());
+                switch (option)
+                {
+                    case ShortcutOption1:
+                        MainPage.Navigation.PushAsync(new DetailPage());
+                        break;
+                    case ShortcutOption2:
+
+                        MainPage.Navigation.PushAsync(new Detail2Page());
+                        break;
+                }
+            }
+            else
+                base.OnAppLinkRequestReceived(uri);
+        }
+    }
+}
